1bd3f948e39f32a11353dc1f572094b23554c647
Christopher Michel <christopher.michel@edu.pucrs.br>
2022-11-07 16:40:49 -0300

    Move initial messages to client application

--- src/client/Client.java
+++ src/client/Client.java
@@ -4,8 +4,19 @@ public class Client {
 
     public static void main(String[] args) {
         try {
+            System.out.println("Voce acabou de entrar um labirinto");
+            Thread.sleep(1000);
+            System.out.println("Existe apenas uma saida e somente o primeiro jogador a passar por ela sobrevive");
+            Thread.sleep(1000);
+            System.out.println("Para iniciar digite ::CREATE_USER <apelido> inserindo um apelido de sua escolha");
+            Thread.sleep(1000);
+            System.out.println("Para verificar os possiveis comandos do jogo digite ::HELP");
+            Thread.sleep(1000);
+            System.out.println("Boa sorte!");
+
             ClientManager cm = new ClientManager();
             cm.executeClient();
+
 
         } catch (Exception e) {
             System.err.println(e.getMessage());
