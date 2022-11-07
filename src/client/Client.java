405f6c6870c0e76136431fc731a4d7f1efaf23b7
Christopher Michel <christopher.michel@edu.pucrs.br>
2022-11-07 19:35:06 -0300

    Fix client message

--- src/client/Client.java
+++ src/client/Client.java
@@ -4,19 +4,17 @@ public class Client {
 
     public static void main(String[] args) {
         try {
-            System.out.println("Você estava explorando uma área perto do lago e percebeu que havia uma pequena caverna na qual decidiu explorar.");
-            Thread.sleep(1000);
-            System.out.println("Logo depois de descer algumas pedras para entrar, percebeu que é impossível voltar pelo mesmo caminho...");
-            Thread.sleep(1000);
-            System.out.println("Sendo assim, o único jeito de sobreviver é atravessando");
-            Thread.sleep(1000);
-            System.out.println("Existe apenas uma saida e somente o primeiro jogador a passar por ela sobrevive");
-            Thread.sleep(1000);
+            System.out.println("Voce esta explorando uma area perto do lago e percebeu que ha uma pequena caverna na qual decide explorar.");
+            Thread.sleep(3000);
+            System.out.println("Logo depois de descer algumas pedras para entrar, percebe que e impossível voltar pelo mesmo caminho...");
+            Thread.sleep(3000);
+            System.out.println("Sendo assim, o unico jeito de sobreviver e atravessando o labirinto");
+            Thread.sleep(3000);
             System.out.println("Para iniciar digite ::CREATE_USER <apelido> inserindo um apelido de sua escolha");
-            Thread.sleep(1000);
+            Thread.sleep(3000);
             System.out.println("Para verificar os possiveis comandos do jogo digite ::HELP");
-            Thread.sleep(1000);
-            System.out.println("Boa sorte!");
+            Thread.sleep(3000);
+            System.out.println("Existe apenas uma saida do labirinto. Boa sorte!");
 
             ClientManager cm = new ClientManager();
             cm.executeClient();
